de8e4896a83da4120cfbaf4392a62df7d1fe1b66
2767206082@qq.com <2767206082@qq.com>
2025-11-27 12:46:46 +0800

    update sam lora

--- segment_anything/build_sam.py
+++ segment_anything/build_sam.py
@@ -241,6 +241,106 @@ def _build_sam_adapter(
 
 
 
+def build_sam_lora_vit_b(checkpoint=None):
+    return _build_sam_lora(
+        encoder_embed_dim=768,
+        encoder_depth=12,
+        encoder_num_heads=12,
+        encoder_global_attn_indexes=[2, 5, 8, 11],
+        checkpoint=checkpoint,
+    )
+
+def _build_sam_lora(
+    encoder_embed_dim,
+    encoder_depth,
+    encoder_num_heads,
+    encoder_global_attn_indexes,
+    checkpoint=None,
+):
+    from .modeling.sam_lora import Sam as SamLoRA
+    prompt_embed_dim = 256
+    image_size = 1024
+    vit_patch_size = 16
+    image_embedding_size = image_size // vit_patch_size
+    sam = SamLoRA(
+        image_encoder=ImageEncoderViT(
+            depth=encoder_depth,
+            embed_dim=encoder_embed_dim,
+            img_size=image_size,
+            mlp_ratio=4,
+            norm_layer=partial(torch.nn.LayerNorm, eps=1e-6),
+            num_heads=encoder_num_heads,
+            patch_size=vit_patch_size,
+            qkv_bias=True,
+            use_rel_pos=True,
+            global_attn_indexes=encoder_global_attn_indexes,
+            window_size=14,
+            out_chans=prompt_embed_dim,
+        ),
+        prompt_encoder=PromptEncoder(
+            embed_dim=prompt_embed_dim,
+            image_embedding_size=(image_embedding_size, image_embedding_size),
+            input_image_size=(image_size, image_size),
+            mask_in_chans=16,
+        ),
+        mask_decoder=MaskDecoder(
+            num_multimask_outputs=3,
+            transformer=TwoWayTransformer(
+                depth=2,
+                embedding_dim=prompt_embed_dim,
+                mlp_dim=2048,
+                num_heads=8,
+            ),
+            transformer_dim=prompt_embed_dim,
+            iou_head_depth=3,
+            iou_head_hidden_dim=256,
+        ),
+        pixel_mean=[123.675, 116.28, 103.53],
+        pixel_std=[58.395, 57.12, 57.375],
+    )
+    sam.eval()
+    checkpoint = Path(checkpoint)
+    if checkpoint.name == "sam_vit_b_01ec64.pth" and not checkpoint.exists():
+        cmd = input("Download sam_vit_b_01ec64.pth from facebook AI? [y]/n: ")
+        if len(cmd) == 0 or cmd.lower() == "y":
+            checkpoint.parent.mkdir(parents=True, exist_ok=True)
+            print("Downloading SAM ViT-B checkpoint...")
+            urllib.request.urlretrieve(
+                "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth",
+                checkpoint,
+            )
+            print(checkpoint.name, " is downloaded!")
+    elif checkpoint.name == "sam_vit_h_4b8939.pth" and not checkpoint.exists():
+        cmd = input("Download sam_vit_h_4b8939.pth from facebook AI? [y]/n: ")
+        if len(cmd) == 0 or cmd.lower() == "y":
+            checkpoint.parent.mkdir(parents=True, exist_ok=True)
+            print("Downloading SAM ViT-H checkpoint...")
+            urllib.request.urlretrieve(
+                "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth",
+                checkpoint,
+            )
+            print(checkpoint.name, " is downloaded!")
+    elif checkpoint.name == "sam_vit_l_0b3195.pth" and not checkpoint.exists():
+        cmd = input("Download sam_vit_l_0b3195.pth from facebook AI? [y]/n: ")
+        if len(cmd) == 0 or cmd.lower() == "y":
+            checkpoint.parent.mkdir(parents=True, exist_ok=True)
+            print("Downloading SAM ViT-L checkpoint...")
+            urllib.request.urlretrieve(
+                "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth",
+                checkpoint,
+            )
+            print(checkpoint.name, " is downloaded!")
+
+    if checkpoint is not None:
+        with open(checkpoint, "rb") as f:
+            state_dict = torch.load(f, map_location=torch.device('cpu'))
+
+        new_state_dict = {}
+        for key, value in state_dict.items():
+            new_key = key.replace("attn.qkv", "attn.qkv.qkv") if "image_encoder.blocks" in key else key
+            new_state_dict[new_key] = value
+        sam.load_state_dict(new_state_dict, strict=False)
+    return sam
 
 sam_model_registry = {
     "default": build_sam_vit_h,
@@ -248,4 +348,5 @@ sam_model_registry = {
     "vit_l": build_sam_vit_l,
     "vit_b": build_sam_vit_b,
     "vit_adapter": build_sam_adapter_vit_b,
+    "vit_lora": build_sam_lora_vit_b,
 }
